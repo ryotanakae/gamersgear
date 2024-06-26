a82094f1b9d69de31b836f2f95cc7425e3f17f19
EC2 Default User <ec2-user@ip-172-31-23-107.ap-northeast-1.compute.internal>
2024-06-26 12:13:41 +0000

    searchバグ修正

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -9,7 +9,10 @@ class Admin::SearchesController < ApplicationController
     when "User"
       @users = User.looks(params[:search], params[:word])
     when "Post"
-      @posts = Post.looks(params[:search], params[:word]).page(params[:page]).per(7)
+      @posts = Post.joins(:user, :category)
+                   .where('posts.title LIKE ? OR posts.body LIKE ? OR users.name LIKE ? OR categories.name LIKE ?', "%#{@word}%", "%#{@word}%", "%#{@word}%", "%#{@word}%")
+                   .page(params[:page])
+                   .per(7)
       @posts = sort_posts(@posts)
     when "PostComment"
       @post_comments = PostComment.joins(:post, :user).where('post_comments.body LIKE ? OR posts.title LIKE ? OR users.name LIKE ?', "%#{@word}%", "%#{@word}%", "%#{@word}%")
@@ -22,19 +25,18 @@ class Admin::SearchesController < ApplicationController
   def sort_posts(posts)
     case params[:sort]
     when 'newest'
-      posts = posts.order(created_at: :desc)
+      posts.order('posts.created_at DESC')
     when 'oldest'
-      posts = posts.order(created_at: :asc)
+      posts.order('posts.created_at ASC')
     when 'highest_rated'
-      posts = posts.order(star: :desc)
+      posts.order('posts.star DESC')
     when 'most_liked'
-      posts = posts.left_joins(:likes).group(:id).order('COUNT(likes.id) DESC')
+      posts.left_joins(:likes).group('posts.id').order('COUNT(likes.id) DESC')
     when 'most_commented'
-      posts = posts.left_joins(:post_comments).group(:id).order('COUNT(post_comments.id) DESC')
+      posts.left_joins(:post_comments).group('posts.id').order('COUNT(post_comments.id) DESC')
     else
       posts
     end
-    posts.page(params[:page]).per(7)
   end
 
 end
