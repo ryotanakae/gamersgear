427e74155d71dc17f8d1de7fda7da0f5bf32f630
EC2 Default User <ec2-user@ip-172-31-23-107.ap-northeast-1.compute.internal>
2024-07-30 17:12:13 +0000

    コメント削除バグ修正

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,7 +1,7 @@
 class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  has_many :notifications, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
   after_create :create_notification
 
@@ -9,6 +9,16 @@ class PostComment < ApplicationRecord
 
   private
     def create_notification
-      post.user.create_notification("comment", self) unless user == post.user
+      Notification.create(
+        user: user,
+        recipient: post.user,
+        action: 'comment',
+        notifiable: self
+      ) unless user == post.user
     end
+    
+    def destroy_related_notifications
+      Notification.where(notifiable_id: id, notifiable_type: 'PostComment').destroy_all
+    end
+  
 end
